76021ab005c8ce792f5bf7d5c020ec98a10b5627
natoine <antoineseilles@gmail.com>
2020-10-22 16:12:00 +0200

    best to consolelog data

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,3 +1,3 @@
 function myfetch(url, headers, cors){
-    fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'}).then(function(response){response.text().then(function(data){console.log(data)})})
+    fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'}).then(function(response){response.text().then(function(data){console.log("data", data)})})
 } 
