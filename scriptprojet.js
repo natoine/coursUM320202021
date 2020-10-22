ba5edff8ae7faeda76804e5f13cc1e3a6c2467ad
natoine <antoineseilles@gmail.com>
2020-10-22 16:37:20 +0200

    awaits and no work on response

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,5 +1,5 @@
 
 function myfetch(url, headers, cors){
-    let fetchresp = await fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'}).then(function(response){response.text().then(function(data){console.log("data", data)})})
+    let fetchresp = await fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'})
     return fetchresp ;
 } 
