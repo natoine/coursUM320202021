9457b463177ff4373c39c94424c717a88be5cc70
claroha <ca.letellier@orange.fr>
2020-10-22 16:24:12 +0200

    ajout de la carte et du css

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,5 @@
 console.log("c'est ici qu'il faut coder votre javascript")
+var main_content = document.getElementById("maincontent");
+var mapdiv = main_content.appendChild(document.createElement("div")).setAttribute("id","map");
+var infodiv = main_content.appendChild(document.createElement("div")).setAttribute("id","info");
+var mymap = L.map('map').setView([51.505, -0.09], 13);
