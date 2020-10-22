33ec03285a222ea4aed1aaedb39fa77e50682b2b
natoine <antoineseilles@gmail.com>
2020-10-22 16:52:38 +0200

    logdata function

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,5 +1,9 @@
 
 async function myfetch(url, headers, cors){
-    let fetchresp = await fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'})
+    let fetchresp = await fetch(url,{method:'GET', headers:headers, mode:cors, cache:'default'});
     return fetchresp ;
 } 
+
+function logdata(fetchresp) {
+    fetchresp.then(function(response){response.text().then(function(data){console.log("data", data)})}) ;
+}
