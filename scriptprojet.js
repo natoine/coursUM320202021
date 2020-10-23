4f08a66057e4058e68f879af499253f6b4896b4c
MinouDu34 <lulu57580@gmail.com>
2020-10-23 14:18:21 +0200

    Ajout d'idée de requêtes pour la Métropole

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,10 @@
 console.log("c'est ici qu'il faut coder votre javascript")
+
+var myHeaders = new Headers({"Access-Control-Allow-Origin" : "*"})
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'no-cors',
+               cache: 'default' };
+
+fetch("https://data.montpellier3m.fr/sites/default/files/ressources/OSM_Castelnau_equipement_sportif_point.json",myInit).then(data=>{data.text().then(data=>{result=data.records})})
