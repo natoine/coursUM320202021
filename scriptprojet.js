a3dc5e1b53efbdd5f78a355387ef80cbf0b9c6db
Amelia ANDRAUD <andraud.amelia@hotmail.com>
2020-10-23 11:54:52 +0200

    ajout d'un fetch et affichage dans le dom

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,11 @@
-console.log("c'est ici qu'il faut coder votre javascript")
+fetch('https://coronavirusapi-france.now.sh/AllLiveData', { method: 'GET',
+               headers: {},
+               mode: 'cors',
+               cache: 'default'}).then(
+    function(response){
+        response.json().then(function(data){
+        	
+        	document.getElementById("maincontent").innerHTML=data["allLiveFranceData"][0]["nouvellesReanimations"];
+        })
+            }
+)
