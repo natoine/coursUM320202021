f90e6e385a6b3d74212345664a66e978e5d77b46
Lucas_Nieder-Fioletti <lulu57580@gmail.com>
2020-11-04 18:19:56 +0100

    script

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,11 +1,20 @@
 console.log("c'est ici qu'il faut coder votre javascript")
 
-fetch("https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-2016-primary-results&q=state%3DTexas&facet=state&facet=county&facet=party&facet=candidate")
+fetch("https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json")
   .then((response) => {
     return response.json()
   })
   .then((data) => {
     // Work with JSON data here
     res=JSON.stringify(data)
-    console.log(res);
+    for (var i  in data){
+      $("#"+i).attr("onmouseover", "drawInfobulle("+data[i]+")");
+      $("#"+i).attr("onmouseleave", "removeInfobulle()");
+    }
   })
+function drawInfobulle(element){
+  console.log(element);
+}
+function removeInfobulle(){
+  console.log('quitter');
+}
