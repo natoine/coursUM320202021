17a8c8964971a0836868bfe6cb26f25726dfe1dc
Lucas_Nieder-Fioletti <lulu57580@gmail.com>
2020-11-05 18:32:32 +0100

    mise à niveau

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,21 +1,50 @@
 console.log("c'est ici qu'il faut coder votre javascript")
 
-fetch("https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json")
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    // Work with JSON data here
-    res=JSON.stringify(data)
-    for (var i  in data){
-      console.log(data);
-      $("#"+i).attr("onmouseover", "drawInfobulle('"+data[i]+"', '#"+i+"')");
-      $("#"+i).attr("onmouseleave", "removeInfobulle()");
-    }
-  })
+function etats(){
+  fetch("https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      // Work with JSON data here
+      res=JSON.stringify(data)
+      for (var i  in data){
+        $("#"+i).attr("onmouseover", "drawInfobulle('"+data[i]+"', '#"+i+"')");
+        $("#"+i).attr("onmouseleave", "removeInfobulle()");
+      }
+    })
+}
+
 function drawInfobulle(element,index){
+  var infobulle = $("#infobulle");
+  var donnees =$('#donnees');
+  infobulle.html(element);
+  fetch("https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-2016-primary-results&q=state%3D'"+element+"'")
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      // Work with JSON data here
+      res=JSON.stringify(data)
+      console.log(data[1]);
+      for (var i  in data['records']){
+        var str="";
+        str+=data['records'][i]['fields']['candidate'];
+        str+=' : ';
+        // //str+=data['records'][i]['fields']['votes'];
+        // str+='Proportion de votes : '+data['records'][i]['fields']['fraction_votes'];
+        // //str+='Comté : '+data['records'][i]['fields']['county'];
+        str+=data['records'][i]['fields']['party'];
+        // str+='\n';
+        $("#liste").append(str);
+      }
+      //donnees.html(str);
+    })
   console.log(element);
 }
 function removeInfobulle(){
+  $("#liste").append('');
+  var donnees =$('#donnees');
+  donnees.html('')
   console.log('quitter');
 }
