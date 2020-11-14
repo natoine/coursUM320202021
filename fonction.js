fe2f9ed4e87817f5e9753db5e29f3bd41ea1fda2
vmelancon <victor.melancon0@gmail.com>
2020-11-14 22:38:38 +0100

    modify sphere to create and destroy whenever filter change

--- fonction.js
+++ fonction.js
@@ -1,97 +1,97 @@
 function formater(txt)
+{
+    /*
+    Methode : On prend un csv sous forme de txt. quand on reprère un espace alors on ajoute le mot dans une liste quand on voit deux espace on creer une nouvelle liste
+    */
+    // console.log(txt);
+    //Mise en forme : txt => tableau
+    mot = "";
+    var liste = [];
+    var listeFinal = [];
+
+    for (var i = 0 ; i <= txt.length; i++) 
     {
-        /*
-        Methode : On prend un csv sous forme de txt. quand on reprère un espace alors on ajoute le mot dans une liste quand on voit deux espace on creer une nouvelle liste
-        */
-
-        //Mise en forme : txt => tableau
-        mot = "";
-        var liste = [];
-        var listeFinal = [];
-
-        for (var i = 0 ; i <= txt.length; i++) 
+        if (txt[i] == '\n')
         {
-            if (txt[i] == '\n')
-            {
-                listeFinal.push(liste);
-                liste= [];
-            }
+            listeFinal.push(liste);
+            liste= [];
+        }
 
-            else if(txt[i] == ',')
-            {
-                liste.push(mot);
-                mot = "";
-            }
+        else if(txt[i] == ',')
+        {
+            liste.push(mot);
+            mot = "";
+        }
 
-            else
-            {
-                mot += txt[i];
-            }
-        }
-        
-        //Supression caractere indésirable 
-        for(var i = 0 ; i <= listeFinal.length - 1 ; i++ )
+        else
         {
-            for(var j = 0 ; j <= listeFinal[i].length - 1; j++)
-            {
-                listeFinal[i][j] = listeFinal[i][j].split('"').join("");
-                listeFinal[i][j] = listeFinal[i][j].split('Flag Codes').join("");
-            }
+            mot += txt[i];
         }
-        
-        
-        //Supression attributs indésirable
-        listeFinal.splice(0,1);
-        
-        for(var i = 0 ; i < listeFinal.length ; i++)
-        {
-            listeFinal[i].splice(1,5);
-        }
-        
-        return listeFinal;
     }
     
-    function concat(l1,l2,l3)
+    //Supression caractere indésirable 
+    for(var i = 0 ; i <= listeFinal.length - 1 ; i++ )
     {
-        //l1 = dette ; l2 = salaire
-        var pays = [];
-
-        //On check la liste la plus courte
-        if(l3.length < l2.length && l3.length < l1.length)
+        for(var j = 0 ; j <= listeFinal[i].length - 1; j++)
         {
-            liste1 = l3;
-            liste2 = l1;
-            liste3 = l2;
+            listeFinal[i][j] = listeFinal[i][j].split('"').join("");
+            listeFinal[i][j] = listeFinal[i][j].split('Flag Codes').join("");
         }
-        else if(l2.length < l1.length && l2.length < l3.length)
-        {
-            liste1 = l2;
-            liste2 = l1;
-            liste3 = l3;
-        }
-        else if(l1.length < l2.length && l1.length < l3.length)
-        {
-            liste1 = l1;
-            liste2 = l2;
-            liste3 = l3;
-        }
-
-
-        //On assemble les données quand les pays sont les memes.
-        for(var i = 0 ; i < liste1.length; i++)
-        {
-            for(var j = 0 ; j < liste2.length ; j++)
-            {
-                for(var n = 0 ; n < liste3.length ; n++)
-                {
-                    if(liste1[i][0] == liste2[j][0] && liste1[i][0] == liste3[n][0])
-                    {
-                        pays.push([ liste1[i][0], liste1[i][1], liste2[j][1], liste3[n][1] ]);
-                    }  
-                }
-                
-            }
-        }
-
-        return pays;
     }
+
+
+    //Supression attributs indésirable
+    listeFinal.splice(0,1);
+
+    for(var i = 0 ; i < listeFinal.length ; i++)
+    {
+        listeFinal[i].splice(1,5);
+    }
+
+    return listeFinal;
+}
+
+function concat(l1,l2,l3)
+{
+    //l1 = dette ; l2 = salaire
+    var pays = [];
+
+    //On check la liste la plus courte
+    if(l3.length < l2.length && l3.length < l1.length)
+    {
+        liste1 = l3;
+        liste2 = l1;
+        liste3 = l2;
+    }
+    else if(l2.length < l1.length && l2.length < l3.length)
+    {
+        liste1 = l2;
+        liste2 = l1;
+        liste3 = l3;
+    }
+    else if(l1.length < l2.length && l1.length < l3.length)
+    {
+        liste1 = l1;
+        liste2 = l2;
+        liste3 = l3;
+    }
+
+
+    //On assemble les données quand les pays sont les memes.
+    for(var i = 0 ; i < liste1.length; i++)
+    {
+        for(var j = 0 ; j < liste2.length ; j++)
+        {
+            for(var n = 0 ; n < liste3.length ; n++)
+            {
+                if(liste1[i][0] == liste2[j][0] && liste1[i][0] == liste3[n][0])
+                {
+                    pays.push({ "country" : liste1[i][0], "debt" : liste1[i][1], "wage" : liste2[j][1], "pib" : liste3[n][1] });
+                }  
+            }
+            
+        }
+    }
+
+    return pays;
+}
